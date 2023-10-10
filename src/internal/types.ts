c5b53d37e2a4e08bbef71d5dbafa7cdc5ebbb08e
Semyon Levin <231188+remal@users.noreply.github.com>
2023-10-10 14:30:14 -0700

    Update types.ts

--- src/internal/types.ts
+++ src/internal/types.ts
@@ -1,4 +1,4 @@
-import { components } from '@octokit/openapi-types'
+import type { components } from '@octokit/openapi-types'
 
 export type PullRequest = components['schemas']['pull-request']
 export type PullRequestSimple = components['schemas']['pull-request-simple']
